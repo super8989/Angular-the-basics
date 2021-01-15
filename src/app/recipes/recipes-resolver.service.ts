1f66be47e72759ab8955c61ba07969f054e2b1b7
Sam <super8989@gmail.com>
2021-01-15 16:05:54 -0500

    Fixing a Bug with the Resolver

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -6,14 +6,27 @@ import {
 } from '@angular/router';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
+  ) {}
 
   // resolver subscribes automatically
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     console.log('resolver running');
-    return this.dataStorageService.fetchRecipes();
+
+    const recipes = this.recipeService.getRecipes();
+
+    if (recipes.length === 0) {
+      console.log('resolver fetching recipes');
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      console.log('resolver not fetching recipes');
+      return recipes;
+    }
   }
 }
