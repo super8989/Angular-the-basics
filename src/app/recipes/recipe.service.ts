ed1868047e1b387628a39cb2a62ef6877bdb5e2d
Sam <super8989@gmail.com>
2021-01-15 15:03:51 -0500

    Transforming Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,21 +8,24 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  // can't access this directly from outside
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Lasagna',
-      'Tomato sauce lasagna with potato chunks',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872',
-      [new Ingredient('Tomato', 3), new Ingredient('Potato', 8)]
-    ),
-    new Recipe(
-      'Pasta',
-      'Fettucini pasta with alfredo sauce',
-      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg',
-      [new Ingredient('Pasta', 2), new Ingredient('Alfredo', 4)]
-    ),
-  ];
+  // // Data being fetched from Firebase
+  // // can't access "private" directly from outside
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Lasagna',
+  //     'Tomato sauce lasagna with potato chunks',
+  //     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872',
+  //     [new Ingredient('Tomato', 3), new Ingredient('Potato', 8)]
+  //   ),
+  //   new Recipe(
+  //     'Pasta',
+  //     'Fettucini pasta with alfredo sauce',
+  //     'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg',
+  //     [new Ingredient('Pasta', 2), new Ingredient('Alfredo', 4)]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { map } from 'rxjs/operators';
 
 // Optional way to provide service to the root (instead of adding in app.module)
 @Injectable({ providedIn: 'root' })
@@ -29,8 +30,19 @@ export class DataStorageService {
       .get<Recipe[]>(
         'https://angular-recipe-book-e8016-default-rtdb.firebaseio.com/recipes.json'
       )
+      .pipe(
+        map((recipes) => {
+          console.log(recipes);
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
       .subscribe((recipes) => {
-        // console.log(recipes);
+        console.log(recipes);
         this.recipesService.setRecipes(recipes);
       });
   }
