2f88f1530b6787af919c0122b28869a5b310d7c8
Sam <super8989@gmail.com>
2021-01-28 15:45:41 -0500

    Dispatching Auth Actions

--- recipeBookNgRx/src/app/auth/auth.service.ts
+++ recipeBookNgRx/src/app/auth/auth.service.ts
@@ -6,6 +6,10 @@ import { catchError, tap } from 'rxjs/operators';
 import { API } from 'src/environments/api.service';
 import { User } from './user.model';
 
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
+
 export interface AuthResponseData {
   kind: string;
   idToken: string;
@@ -25,7 +29,8 @@ export class AuthService {
   constructor(
     private http: HttpClient,
     private api: API,
-    private router: Router
+    private router: Router,
+    private store: Store<fromApp.AppState>
   ) {}
 
   signup(email: string, password: string) {
@@ -106,7 +111,16 @@ export class AuthService {
 
     if (loadedUser.token) {
       console.log('loadedUser', loadedUser);
-      this.user.next(loadedUser);
+
+      // this.user.next(loadedUser);
+      this.store.dispatch(
+        new AuthActions.Login({
+          email: loadedUser.email,
+          userId: loadedUser.id,
+          token: loadedUser.token,
+          expirationDate: new Date(userData._tokenExpirationDate),
+        })
+      );
 
       const expirationDuration =
         new Date(userData._tokenExpirationDate).getTime() -
@@ -120,7 +134,9 @@ export class AuthService {
 
   logout() {
     console.log('logged out', this.user);
-    this.user.next(null);
+    // this.user.next(null);
+    this.store.dispatch(new AuthActions.Logout());
+
     this.router.navigate(['/auth']);
     localStorage.removeItem('userData');
 
@@ -149,7 +165,21 @@ export class AuthService {
     const user = new User(email, userId, token, expirationDate);
 
     console.log('user authenticated', user);
-    this.user.next(user);
+
+    // this.user.next(user);
+    this.store.dispatch(
+      new AuthActions.Login({
+        // email: email,
+        // userId: userId,
+        // token: token,
+        // expirationDate: expirationDate,
+        email,
+        userId,
+        token,
+        expirationDate,
+      })
+    );
+
     // run autoLogout
     this.autoLogout(expiresIn * 1000);
 
