e42aaf8726f2b75787d38b80841301c1de25b38e
Sam <super8989@gmail.com>
2021-01-28 21:14:40 -0500

    Effects & Error Handling

--- recipeBookNgRx/src/app/auth/store/auth.effects.ts
+++ recipeBookNgRx/src/app/auth/store/auth.effects.ts
@@ -1,9 +1,53 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { API } from 'src/environments/api.service';
 
 import * as AuthActions from './auth.actions';
 
-export class AuthEffects {
-  authLogin = this.actions$.pipe(ofType(AuthActions.LOGIN_START));
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
 
-  constructor(private actions$: Actions) {}
+export class AuthEffects {
+  @Effect()
+  apiKey = this.api.apikey;
+
+  authLogin = this.actions$.pipe(
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      console.log(authData);
+
+      return this.http
+        .post<AuthResponseData>(
+          `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.apiKey}`,
+          {
+            email: authData.payload.email,
+            password: authData.payload.password,
+            returnSecureToken: true,
+          }
+        )
+        .pipe(
+          catchError((error) => {
+            of();
+          }),
+          map((resData) => {
+            of();
+          })
+        );
+    })
+  );
+
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private api: API
+  ) {}
 }
